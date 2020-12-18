040c06198d186871fdd4d7fba5680bc217333c15
Hope Dorkenoo <compounddork@gmail.com>
2020-12-18 14:23:24 +0000

    Look at Request and Response Objects

--- buildServer/server.js
+++ buildServer/server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    //dealing with responses
+    res.setHeader('Content-type', 'text/html');
+
+    res.write('<h1>hello ,world</h1>');
+    res.write('<h2>hello again,world</h2>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
