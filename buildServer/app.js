445b3f2f97ad8d07212d18929995575a22b59295
Hope Dorkenoo <compounddork@gmail.com>
2020-12-19 19:37:55 +0000

    Use View and Express Render

--- buildServer/app.js
+++ buildServer/app.js
@@ -3,29 +3,34 @@ const express = require("express");
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+app.set('views', './buildServer/views');
+
+
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index', { title: 'Home' });
 });
 
 //about call
 app.get('/about', (req, res) => {
     // res.send('');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
 });
 
 //redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create' });
 });
 
 //404 requests
 app.use((req, res) => {
 
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404' });
 })
 // app.get('/', (req, res) => {
 //     res.send('<p>home page</p>');
