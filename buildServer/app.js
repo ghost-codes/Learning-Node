7798eb32cf6261fd7d7ae7db21dd7aa9a6e7028c
Hope Dorkenoo <compounddork@gmail.com>
2020-12-20 14:59:19 +0000

    Learn Middleware and third Party Middleware Morgan

--- buildServer/app.js
+++ buildServer/app.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 //express app
 const app = express();
 
+//connect to mongo db
+const dbURI = 'mongodb+srv://admin:test1234@nodetuts.pxhce.mongodb.net/nodeTuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        app.listen(3000);
+    }).catch((err) => console.log(err));
 //register view engine
 app.set('view engine', 'ejs');
 app.set('views', './buildServer/views');
 
 
 //listen for requests
-app.listen(3000);
+
+
+app.use(express.static('buildServer/public'))
+app.use(morgan('dev'));
+
 
 app.get('/', (req, res) => {
     const blogs = [
