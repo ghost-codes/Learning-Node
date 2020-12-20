0c5739f2b8d59af36f115c0524e77c7a7f2ae877
Hope Dorkenoo <compounddork@gmail.com>
2020-12-20 15:30:29 +0000

    Fetch Data from DB and rendered

--- buildServer/app.js
+++ buildServer/app.js
@@ -24,34 +24,8 @@ app.use(express.static('buildServer/public'))
 app.use(morgan('dev'));
 
 
-//mongoose and mongo sandbox routes
-
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new Blog',
-        snippet: 'about my new blog',
-        body: 'Duis deserunt ipsum aliqua voluptate incididunt est elit labore duis proident. Dolore mollit non est irure proident est veniam voluptate. Eu non sunt sint ea ullamco occaecat cillum sunt nulla adipisicing est officia. Laboris ullamco laboris labore laborum cupidatat laborum officia sint amet pariatur.'
-    });
-
-    blog.save().then(result => res.send(result)).catch(err => console.log(err));
-});
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find().then(result => res.send(result)).catch(err => console.log(err));
-});
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('5fdf69e513b5844504ff37e9')
-        .then(result => res.send(result)).catch(err => console.log(err));
-})
-
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-    ];
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
 //about call
@@ -60,7 +34,17 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
 
-//redirect
+
+//blogs route
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then(result => {
+            res.render('index', { title: "All Blogs", blogs: result });
+        }).catch(err => console.log(err));
+});
+
+
+//create blogs
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create New Blog' });
 });
